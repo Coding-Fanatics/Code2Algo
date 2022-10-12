d4b8e057aa1b70aec6a1d1610df6fe03a4edf3a4
AnsahMohammad <mohdansah10@gmail.com>
2022-10-12 08:16:51 +0530

    Updated Readme and File Name

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Code2Algo
 Code to algorithm
+This is a compiler which converts Python code into Algorithm.
--- tempCodeRunnerFile.py
+++
@@ -1,6 +0,0 @@
-elif(len(range_)==2):
-            #     fin = "repeat step {} to {} for {}={} until {}={} step +1".format(start_step+1,next_step,variable,variable,range_[0],range[1])
-            # elif(len(range_)==3):
-            #     fin = "repeat step {} to {} for {}={} until {}=10 step +1".format(start_step+1,next_step,variable,variable,range_[0],range[1])
-            # else:
-            #     raise "ValueError"
