1dd94d181590548465f95a7de8935de68b80bf5e
Garvit Shah <garvit.shah3@gmail.com>
2022-10-15 06:53:45 +0530

    Conditionals Fixed

--- Categorising.py
+++ Categorising.py
@@ -11,9 +11,10 @@ elif a==b:
     print("yes")    
 else:
     print()  
-for i in range(10):
     print('a')    
 '''
+# for i in range(10):
+
 
 
 
@@ -24,23 +25,25 @@ def Tokeniser(l):
     LoRegx = re.findall("^for+.*:$|^while+.*:$", l)
     IndRegx = re.findall("^ +", l)
     ElseRegx = re.findall("^else", l)
-    if CoRegx:
+    if len(IndRegx):
+        return 4
+    elif len(CoRegx):
         # print("Condition: ", CoRegx)
         return 0
-    elif LoRegx:
+    elif len(LoRegx):
         # print("Loop: ", LoRegx)
         return 1    
-    elif FnRegx:
+    elif len(FnRegx):
         # print(i, " --------------- ")
         # print("Funciton: ", FnRegx)
         return 2
-    elif OpRegx:
+    elif len(OpRegx):
         # print("Operational: ", OpRegx)
         return 3
-    elif IndRegx:
-        return 4 
     elif ElseRegx:
         return 5       
+    else:
+        return -1          
 
 def conditionTranslator(i):
     if_elif_else = re.findall("if|elif|else", lines[i])
@@ -64,41 +67,37 @@ def conditionTranslator(i):
         return op1
     except:
         print("Error", i)    
-    # op1 = op1[1]
-    # op1 = op1.split(">")
-    # op1 = " greater than ".join(op1)
-    # op1 = op1.split("<")
-    # op1 = " less than ".join(op1)
-    # op1 = op1.split("<=")
-    # op1 = " less than or equal to ".join(op1)
-    # op1 = op1.split(">=")
-    # op1 = " greater than or equal to ".join(op1)
-    # op1 = op1.split("!=")
-    # op1 = " not equal to ".join(op1)
-    # op1 = op1.split("==")
-    # op1 = " equal to ".join(op1)  
-    # op1 = op1.split(":")
-    # op1 = op1[0]
-    # return op1
 
 
-def controller(type, i):
+def controller(type, i, level=0):
     if type == 3:
-        op(i)
+        if level == 0:
+            op(i)
         return 1
     elif type == 0:
-        j = i
-        while((type == 4 or type != 5)and j<len(lines)):
-            type = Tokeniser(lines[j])
-            j+=1
-        co(i, j)    
-        return j-i
+        if level == 0:
+            j = Checker(i+1, level+1)
+            co(i, i+j)
+        else:     
+            return 1    
+        # j = i
+        # while((type == 4 or type != 5)and j<len(lines)):
+        #     type = Tokeniser(lines[j])
+        #     j+=1
+        # co(i, j)    
+        # return j-i
     elif type == 2:
-        fn(i)
+        if level == 0:
+            fn(i)
+        return 1
     elif type == 1:
         pass
-
-    return 1    
+    elif type == 4:
+        lines[i] = lines[i].lstrip()
+        # print("--------")
+        return 1+Checker(i+1, level)
+    else:
+        return 1    
 
     
 
@@ -114,18 +113,18 @@ def op(i):                              # Prints the Operational Statements
     print(str(i)+". "+ lines[i])
 
 def co(i1, i2):
-    if_elif_else = re.findall("if|elif|else", lines[i])
-    op1 = conditionTranslator(i)
+    if_elif_else = re.findall("if|elif|else", lines[i1])
+    op1 = conditionTranslator(i1)
     if(if_elif_else[0] != "else"):
-        print(str(i)+". " + if_elif_else[0] + " " + op1 + " goto step-" + str(i1+1) + " else goto step-" + str(i2))
+        print(str(i1)+". " + if_elif_else[0] + " " + op1 + " goto step-" + str(i1+1) + " else goto step-" + str(i2))
     else:
-        print(str(i)+". " + if_elif_else[0] + " " + op1 + " goto step-" + str(i1+1))            
+        print(str(i1)+". " + if_elif_else[0] + " " + op1 + " goto step-" + str(i1+1))            
     
 
 
-def Checker(i=0):                        # Checker Goes line by line checking and sending line to tokensier
+def Checker(i=0, level = 0):                      # Checker Goes line by line checking and sending line to tokensier
     type = Tokeniser(lines[i]) 
-    n = controller(type, i)
+    n = controller(type, i, level)
     return n
     
 
