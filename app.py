d57783e0e991b1c07952e9ab7494ba4dc1379644
Garvit Shah <garvit.shah3@gmail.com>
2022-12-29 18:51:05 +0530

    About Page linked

--- app.py
+++ app.py
@@ -14,6 +14,9 @@ def comb():
     else:
         return render_template("index.html",content=['',''])
 
+@app.route("/About")
+def about():
+    return render_template("About.html")
 
 if __name__ == "__main__":
     app.run(debug = True)
