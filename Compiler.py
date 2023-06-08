96503b3c5c14924a02cbecc78e2b8ef3bb508934
AnsahMohammad <mohdansah10@gmail.com>
2023-06-08 19:51:03 +0530

    cleaned the code and updated Readme.md

--- Compiler.py
+++ Compiler.py
@@ -1,49 +1,9 @@
+"""Compiler module to convert python to algorithm"""
 import re
-
-code = """a = 3 > 4
-a=5+3
-b = 5+3
-def printHi(name):
-    print("Hi "+name)
-    a = 34+2
-c = a*b
-list(c)
-set(a)
-if a > 2:
-    print("Hello")
-elif a < 2:
-    print("Hih")
-    if a == 2:
-        print(2)
-    else:
-        e = 3    
-else:
-    s = 2 
-if 2<5:
-    s = s+1               
-while c > d:
-    a = 33+45
-    b =  a+v
-    while a == b:
-        d = a+3
-    c = 3
-def factorial(n):
-    if n==1:
-        a = 4
-    x = n*n-1
-for i in range(1,10,1):
-    a = 3
-    d = 34+7
-for each_word in lambda:
-    for each in each_word:
-        print("Ansah is my Name")
-        a = ansah
-    k = 5 + 2
-    a = 6*7
-d = 97"""
 
 
 class AlgoCompiler:
+    """Compiler class for the algorithm"""
     algorithm = ""
     f_index = 1
     funcdefs = []
@@ -53,6 +13,7 @@ class AlgoCompiler:
         self.lines = source.split("\n")
 
     def Tokeniser(self, l):
+        """Tokenizer function to recognize the type of line"""
         OpRegx = re.findall(
             "^[a-zA-Z_][a-zA-Z0-9_]*[ ]*=[ ]*.+|^ +[a-zA-Z_][a-zA-Z0-9_]*[ ]*=[ ]*.+", l
         )
@@ -62,38 +23,41 @@ class AlgoCompiler:
         CoRegx = re.findall(
             "^if+.*:$|^else+.*:$|^elif+.*:$|^ +if+.*:$|^ +else+.*:$|^ +elif+.*:$", l
         )
-        LoRegx = re.findall("^for+.*:$|^while+.*:$|^ +while+.*:$|^ +for+.*:$", l)
+        LoRegx = re.findall(
+            "^for+.*:$|^while+.*:$|^ +while+.*:$|^ +for+.*:$", l)
         FunRegx = re.findall("^def .*:$", l.lstrip())
         ComRegx = re.findall("^#", l)
         BlankRegx = re.findall("^ +", l)
         if CoRegx:
             return 0
-        elif LoRegx:
+        if LoRegx:
             return 1
-        elif FunRegx:
+        if FunRegx:
             return 6
-        elif FnRegx:
+        if FnRegx:
             return 2
-        elif OpRegx:
+        if OpRegx:
             return 3
-        elif ComRegx:
+        if ComRegx:
             return 4
-        elif BlankRegx or l == "":
+        if BlankRegx or l == "":
             return 5
-        else:
-            if "while" in l.lstrip()[:5] or "for" in l.lstrip()[:3]:
-                return 1
-            elif (
-                "if" in l.lstrip()[:2]
-                or "else" in l.lstrip()[:4]
-                or "elif" in l.lstrip()[:4]
-            ):
-                return 0
-            # if "def" in l.lstrip()[:3]:
-            #     return 6
-            return -1
+
+        if "while" in l.lstrip()[:5] or "for" in l.lstrip()[:3]:
+            return 1
+        if (
+            "if" in l.lstrip()[:2]
+            or "else" in l.lstrip()[:4]
+            or "elif" in l.lstrip()[:4]
+        ):
+            return 0
+        return -1
 
     def level(self, i):
+        """
+        Level to identify the level inendation
+        primarily helpful in identifying the block of codes
+        """
         level = 0
         for eachLett in self.lines[i]:
             if eachLett == " ":
@@ -104,6 +68,7 @@ class AlgoCompiler:
         return level
 
     def writer(self, compiled_, mode=1):
+        """"Code writing handler"""
         if mode == 0:
             self.f_index = 0
             self.algorithm = ""
@@ -117,6 +82,7 @@ class AlgoCompiler:
             self.f_index += 1
 
     def Translate(self, op):
+        """translator for converting code to english"""
         op = op.split(">")
         op = "greater than".join(op)
         op = op.split("<")
@@ -134,10 +100,15 @@ class AlgoCompiler:
         # raise ValueError("There's an error in line "+str(i))
 
     def operatorParser(self, i):
+        """parser which translates operands to english"""
         compiled_ = self.Translate(self.lines[i].lstrip())
         self.writer(compiled_)
 
     def loopsParser(self, i):
+        """
+        Python loop parser
+        handles the parsing of : while, for loops
+        """
         initialLevel = self.level(i)
         start_step = i
         temp_line = i + 1
@@ -155,7 +126,7 @@ class AlgoCompiler:
             fin = "repeat step {} to {} ".format(start_step + 2, next_step + 1)
 
         if whileReg:
-            condition_ = self.lines[i][initialLevel + 5 :].lstrip()
+            condition_ = self.lines[i][initialLevel + 5:].lstrip()
             fin += " while {}".format(condition_)
             self.writer(self.Translate(fin))
         elif forReg:
@@ -178,7 +149,8 @@ class AlgoCompiler:
                     )
                 elif len(range_) == 3:
                     fin += "for {}={} to {}={} step {}".format(
-                        variable, (range_[0]), variable, int(range_[1]) - 1, range_[2]
+                        variable, (range_[0]), variable, int(
+                            range_[1]) - 1, range_[2]
                     )
                 self.writer(self.Translate(fin))
             else:
@@ -190,6 +162,10 @@ class AlgoCompiler:
                 self.writer(self.Translate(fin))
 
     def functionParser(self, i):
+        """
+        Python function parser
+        handles common functions such as print(), set() etc
+        """
         line = self.lines[i].lstrip()
         general_func = ["print", "set", "list", "dictionary"]
         functionName = re.findall(
@@ -197,10 +173,10 @@ class AlgoCompiler:
         )[0]
         fin = functionName
         if functionName in general_func:
-            functionContent = line[len(functionName) + 1 : -1]
+            functionContent = line[len(functionName) + 1: -1]
             fin += " " + functionContent
         else:
-            functionContent = line[len(functionName) :]
+            functionContent = line[len(functionName):]
             fin += " " + functionContent
         self.writer(self.Translate(fin))
 
@@ -211,12 +187,16 @@ class AlgoCompiler:
     #     self.writer(line)
 
     def FunCollect(self, i):
+        """
+        Python functions collector
+        identifies python and appends to function list
+        new saperate function generation can be done by extracting from function list
+        """
         line = self.lines[i].lstrip()
         # functionName = re.findall("^[a-zA-Z_][a-zA-Z0-9_]*|^ +[a-zA-Z_][a-zA-Z0-9_]*", line)[0]
         functionName = re.findall("^def [a-zA-Z_].*", line)[0][4:-1]
         funcLines = []
         init_level = self.level(i)
-        temp_level = self.level(i + 1)
         temp_line = i + 1
         while self.level(temp_line) > init_level and temp_line < len(self.lines):
             funcLines.append(self.lines[temp_line])
@@ -224,6 +204,10 @@ class AlgoCompiler:
         self.funcdefs.append((functionName, funcLines))
 
     def conditionParser(self, i):
+        """
+        Python conditions parser
+        handles : if, elif, else
+        """
         initialLevel = self.level(i)
         start_step = i
         temp_line = i + 1
@@ -241,7 +225,7 @@ class AlgoCompiler:
         _elifRegx = re.findall("^elif|^ +elif", self.lines[next_step + 1])
 
         if len(ifRegx):
-            condition_ = self.lines[i][initialLevel + 2 :].lstrip()
+            condition_ = self.lines[i][initialLevel + 2:].lstrip()
             fin = "if {} ".format(condition_)
             if len(_elifRegx) or len(_elseRegx):
                 fin += "- go to step {} else to {}".format(
@@ -251,7 +235,7 @@ class AlgoCompiler:
                 fin += "- go to step {}".format(start_step + 2)
             # self.writer(self.Translate(fin))
         elif len(elifRegx):
-            condition_ = self.lines[i][initialLevel + 4 :].lstrip()
+            condition_ = self.lines[i][initialLevel + 4:].lstrip()
             fin = "Else if {} ".format(condition_)
             if len(_elseRegx):
                 fin += "- go to step {} else to {}".format(
@@ -267,6 +251,10 @@ class AlgoCompiler:
         self.writer(self.Translate(fin))
 
     def compile(self):
+        """
+        primary compiler
+        goes line-by-line
+        """
         funcs = self.compile1()
         algorithm1 = self.returnOut()
         for each in funcs:
@@ -278,6 +266,10 @@ class AlgoCompiler:
         self.algorithm = algorithm1
 
     def compile1(self):
+        """
+        Secondary compiler
+        identifies tokens and compiles accordingly
+        """
         i = 0
         while i < len(self.lines):
             a = self.Tokeniser(self.lines[i])
@@ -301,17 +293,13 @@ class AlgoCompiler:
         return self.funcdefs
 
     def returnOut(self, fileExt=""):
+        """Return output, to use the generated algorithm in other functions"""
         return fileExt + self.algorithm
 
     def printOut(self):
+        """display the generated algorithm"""
         print(self.algorithm)
 
     def printFunCollect(self):
+        """display collected funciton"""
         print(self.funcdefs)
-
-
-# model = AlgoCompiler(code)
-# model.compile()
-# algorithm = model.returnOut()
-# # model.printFunCollect()
-# print(algorithm)
--- README.md
+++ README.md
@@ -3,3 +3,55 @@ Code to algorithm
 This is a compiler which converts Python code into an Algorithm.
 
 This Compiler helps in distribution of Codes written in Python for non-Python programmers.
+
+## Tutorial
+```python
+
+# example code
+code = """a = 3 > 4
+a=5+3
+b = 5+3
+def printHi(name):
+    print("Hi "+name)
+    a = 34+2
+c = a*b
+list(c)
+set(a)
+if a > 2:
+    print("Hello")
+elif a < 2:
+    print("Hih")
+    if a == 2:
+        print(2)
+    else:
+        e = 3    
+else:
+    s = 2 
+if 2<5:
+    s = s+1               
+while c > d:
+    a = 33+45
+    b =  a+v
+    while a == b:
+        d = a+3
+    c = 3
+def factorial(n):
+    if n==1:
+        a = 4
+    x = n*n-1
+for i in range(1,10,1):
+    a = 3
+    d = 34+7
+for each_word in lambda:
+    for each in each_word:
+        print("Ansah is my Name")
+        a = ansah
+    k = 5 + 2
+    a = 6*7
+d = 97"""
+
+model = AlgoCompiler(code)
+model.compile()
+algorithm = model.returnOut()
+print(algorithm)
+```
