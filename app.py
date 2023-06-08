4692f167c9cb3ef21e92706cf54901766fc31056
AnsahMohammad <mohdansah10@gmail.com>
2023-06-08 19:54:40 +0530

    cleaned app.py

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask, redirect, url_for, render_template, request
+"""Web app for compiler.py"""
+from flask import Flask, render_template, request
 from Compiler import AlgoCompiler
 
 app = Flask(__name__)
@@ -6,14 +7,18 @@ app = Flask(__name__)
 
 @app.route("/", methods=["POST", "GET"])
 def comb():
+    """
+    Main handler for the request
+    input : Fetches the code in Python
+    output : Outputs Algorithm of the input
+    """
     if request.method == "POST":
         code = request.form["source"]
         model = AlgoCompiler(code)
         model.compile()
         algorithm = model.returnOut()
         return render_template("index.html", content=[code, algorithm])
-    else:
-        return render_template("index.html", content=["", ""])
+    return render_template("index.html", content=["", ""])
 
 
 if __name__ == "__main__":
